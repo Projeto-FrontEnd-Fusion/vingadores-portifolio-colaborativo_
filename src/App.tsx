b3b78b8860e009c3cf7d5cf75699b460fcec8d9f
claudio silva <claudiosilva.one@gmail.com>
2024-12-30 21:08:39 -0300

    chore: remove-default-vite-files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-semibold">Welcome avangers</h1> <br /> 🦾⚙️
+      ⚡🔨 💪💚 🐜🧪 🐝✨ 🛡️🇺🇸 🎯🏹 🕷️🖤 🔮❤️ 🤖💎 🐾🖤 🕸️🕷️ 🌀🧙‍♂️ 🌟🚀
+    </>
+  );
+}
+
+export default App;
