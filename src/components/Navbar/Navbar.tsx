0bd31070738f190067613b93e2aae8234f0f323a
Maria de Fátima <mnunesalves334@gmail.com>
2025-01-01 17:01:29 -0300

    feat: estilizar os links com tailwind

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,39 @@ import React from 'react';
 function Navbar(): JSX.Element {
   return (
     <nav className="flex w-full justify-end items-center">
-      <ul className="flex h-full items-center">
-        <li>
-          <a className='px-4 text-gray-50' href="#equipes">equipes</a>
+      <ul className="flex h-full items-center space-x-6">
+        <li className="relative">
+          <a
+            className="text-gray-50 inline-flex relative transition-colors duration-300 ease-in-out hover:text-blue-300 
+                       after:content-[''] after:absolute after:bottom-[-4px] after:left-0 
+                       after:h-[2px] after:bg-blue-300 after:transition-transform after:duration-300 
+                       after:w-0 hover:after:w-full"
+            href="#equipes"
+          >
+            equipes
+          </a>
         </li>
-        <li>
-          <a className='px-4 text-gray-50' href="#projetos">projetos</a>
+        <li className="relative">
+          <a
+            className="text-gray-50 inline-flex relative transition-colors duration-300 ease-in-out hover:text-blue-300 
+                       after:content-[''] after:absolute after:bottom-[-4px] after:left-0 
+                       after:h-[2px] after:bg-blue-300 after:transition-transform after:duration-300 
+                       after:w-0 hover:after:w-full"
+            href="#projetos"
+          >
+            projetos
+          </a>
         </li>
-        <li>
-          <a className="px-4 text-gray-50" href="#tecnologias">tecnologias</a>
+        <li className="relative">
+          <a
+            className="text-gray-50 inline-flex relative transition-colors duration-300 ease-in-out hover:text-blue-300 
+                       after:content-[''] after:absolute after:bottom-[-4px] after:left-0 
+                       after:h-[2px] after:bg-blue-300 after:transition-transform after:duration-300 
+                       after:w-0 hover:after:w-full"
+            href="#tecnologias"
+          >
+            tecnologias
+          </a>
         </li>
       </ul>
     </nav>
