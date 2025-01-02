59aba6a9a520cccf174ec5ee0e2ffa8a828b4140
Maria de Fátima <mnunesalves334@gmail.com>
2025-01-02 10:46:49 -0300

    feat: adicionar logotipo do frontend fusion

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Navbar(): JSX.Element {
   return (
-    <nav className="flex w-full justify-end items-center">
-      <ul className="flex h-full items-center space-x-6">
+    <nav className="flex w-full items-center justify-between">
+      {/* Logotipo alinhado ao canto superior esquerdo */}
+      <div className="absolute top-0 left-0 p-4">
+          <img
+            src="/assets/logotipo_frontend_fusion.png"
+            alt="Logotipo Frontend Fusion"
+            className="h-8 px-3" // Ajuste o tamanho conforme necessário
+          />
+      </div>
+
+      {/* Links alinhados à direita */}
+      <ul className="flex h-full items-center space-x-6 ml-auto">
         <li className="relative">
           <a
             className="text-gray-50 inline-flex relative transition-colors duration-300 ease-in-out hover:text-blue-300 
