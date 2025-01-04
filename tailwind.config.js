f3a694fa7b2c227ec98a930e324ad6cd63fe00c0
Maria de Fátima <mnunesalves334@gmail.com>
2025-01-04 14:28:28 -0300

    feat: criar a seção de descrição do formulário e configuração de fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fusion': ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
