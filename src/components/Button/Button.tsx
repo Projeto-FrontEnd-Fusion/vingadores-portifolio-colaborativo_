e2730d1b9b936cde400388b5a71bd9fdfe454190
Breno-Mateus <brenomateus36@gmail.com>
2025-01-05 09:30:42 -0300

    feat: criar componente button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,12 @@ interface ButtonProps extends ComponentProps<"a"> {
 export const Button = ({ isNavbar, ...props } :  ButtonProps) => {
     return <a 
             {...props} 
-            className={`bg-colorButton text-colorTextButton font-normal text-center hover:bg-[#23CFCE] active:bg-[#1A9998] ${isNavbar ? "w-[10.313rem] h-[2.563rem] rounded-[2.125rem] py-3 px-4 text-sm leading-[1.05rem]" : "w-[13.625rem] h-12 rounded-3xl py-3 px-8 text-xl leading-6"}`} 
+            className={`bg-colorButton text-colorTextButton font-normal text-center transition duration-300 ease-in-out
+                    hover:bg-[#23CFCE] 
+                    active:bg-[#1A9998] 
+                    ${isNavbar 
+                        ? "w-[10.313rem] h-[2.563rem] rounded-[2.125rem] py-3 px-4 text-sm leading-[1.05rem]" 
+                        : "w-[13.625rem] h-12 rounded-3xl py-3 px-8 text-xl leading-6"}`} 
             href="#formulario" 
             aria-label="Ir para o formulário de inscrição"
             >
