d9a2711e9363ce088b8f3cb549b49e36e2954bce
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-01-09 19:08:24 -0300

    style: identação de 2 espaços

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,21 @@
 import { ComponentProps } from "react";
 
 interface ButtonProps extends ComponentProps<"a"> {
-    isNavbar: boolean;
+  isNavbar: boolean;
 };
 
-export const Button = ({ isNavbar, ...props } :  ButtonProps) => {
-    return <a 
-            {...props} 
-                    hover:bg-[#23CFCE] 
-                    active:bg-[#1A9998] 
-                    ${isNavbar 
-                        ? "w-[10.313rem] h-[2.563rem] rounded-[2.125rem] py-3 px-4 text-sm leading-[1.05rem]" 
-                        : "w-[13.625rem] h-12 rounded-3xl py-3 px-8 text-xl leading-6"}`} 
-            href="#formulario" 
-            aria-label="Ir para o formulário de inscrição"
-            >
-                Inscreva-se aqui
-            </a>;
-};    className={`bg-[#26EEEF] text-[#1A1756] font-normal text-center transition duration-300 ease-in-out
+export const Button = ({ isNavbar, ...props }: ButtonProps) => {
+  return <a
+    {...props}
+    className={`bg-[#26EEEF] text-[#1A1756] font-normal text-center transition duration-300 ease-in-out
+      hover:bg-[#1CC4D4] 
+      active:bg-[#1A9998] 
+      ${isNavbar
+        ? "w-[10.313rem] h-[2.563rem] rounded-[2.125rem] py-3 px-4 text-sm leading-[1.05rem]"
+        : "w-[13.625rem] h-12 rounded-3xl py-3 px-8 text-xl leading-6"}`}
+    href="#formulario"
+    aria-label="Ir para o formulário de inscrição"
+  >
+    Inscreva-se aqui
+  </a>;
+};
