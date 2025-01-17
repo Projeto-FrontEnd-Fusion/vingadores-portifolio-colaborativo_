5ff05336487a1dc6c403ad25c560b788b30f51ae
Breno-Mateus <brenomateus36@gmail.com>
2025-01-17 12:23:50 -0300

    feat: ajustando responsividade da seção de apresentação

--- src/components/SectionPresentation/SectionPresentation.tsx
+++ src/components/SectionPresentation/SectionPresentation.tsx
@@ -2,32 +2,32 @@ import { Button } from "../Button/Button";
 
 const SectionPresentation = () => {
   return (
-    <section className="text-white flex items-center flex-col gap-[2.813rem] px-[0.313rem] py-[2.813rem]
-                            md:flex-row md:items-start md:px-[2.844rem] md:pt-24 md:pb-[11.125rem] md:gap-24"
+    <section className="text-white flex items-center flex-col gap-11 px-1 py-11
+                            lg:flex-row lg:items-start lg:px-11 lg:pt-24 lg:pb-44 lg:gap-24"
     >
-      <div className="flex flex-col gap-8 items-center px-[0.656rem]
-                            md:w-1/2 md:px-0"
+      <div className="flex flex-col gap-8 items-center px-2
+                            lg:w-1/2 lg:px-0"
       >
-        <p className="font-presentation text-base font-normal leading-[1.2rem] text-center">MEET THE</p>
+        <p className="font-presentation text-base font-normal leading-5 text-center">MEET THE</p>
         <img src="/assets/FRONTENDFUSION_2.png" />
-        <p className="font-fusion text-[0.875rem] font-normal leading-[1.313rem] text-center
-                              md:text-[0.938rem] md:leading-[1.406rem]"
+        <p className="font-fusion text-sm font-normal leading-5 text-center
+                              lg:text-base lg:leading-6"
         >Junte-se a nós como voluntário e colabore na criação de uma experiência de aprendizado inovadora para tecnologias web</p>
         <Button isNavbar={false} />
       </div>
 
-      <div className="flex flex-col gap-4 px-[1.594rem]
-                            md:px-0 md:w-1/2"
+      <div className="flex flex-col gap-4
+                            lg:w-1/2"
       >
-        <h2 className="text-[#646DF0] font-fusion text-[2rem] font-medium leading-[2.4rem] text-center
-                              md:text-left md:text-[2.5rem] md:leading-[3rem]"
+        <h2 className="text-[#646DF0] font-fusion text-[2rem] font-medium leading-[3rem] text-center
+                              lg:text-left lg:text-[2.5rem] lg:leading-[3rem] lg:tracking-wide"
         >O que é o Frontend Fusion</h2>
         <p className="font-fusion text-base font-normal text-center
-                              md:text-left"
+                              lg:text-left"
         >Nossa missão é a criação de uma plataforma de aprendizagem em desenvolvimento backend e frontend gratuita e acessível para todos. Desde 2023, nosso voluntariado já alcançou números incríveis.</p>
 
         <div className="flex flex-col gap-12 pt-16
-                                md:pt-6"
+                                lg:pt-6"
         >
           <div className="flex gap-6 items-center">
             <p className="font-fusion text-2xl font-bold leading-9 text-center">+100</p>
