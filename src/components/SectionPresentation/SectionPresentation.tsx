13b1460bbbc464fbad9446189d0156a336693d81
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-01-18 00:09:03 -0300

    feat: adiciona `h1` entre `img` do título da página. Utiliza `alt` para otimização de SEO

--- src/components/SectionPresentation/SectionPresentation.tsx
+++ src/components/SectionPresentation/SectionPresentation.tsx
@@ -9,7 +9,9 @@ const SectionPresentation = () => {
                             lg:w-1/2 lg:px-0"
       >
         <p className="font-presentation text-base font-normal leading-5 text-center">MEET THE</p>
-        <img src="/assets/FRONTENDFUSION_2.png" />
+        <h1>
+          <img src="/assets/FRONTENDFUSION_2.png" alt="Frontend Fusion" />
+        </h1>
         <p className="font-fusion text-sm font-normal leading-5 text-center
                               lg:text-base lg:leading-6"
         >Junte-se a nós como voluntário e colabore na criação de uma experiência de aprendizado inovadora para tecnologias web</p>
@@ -22,19 +24,17 @@ const SectionPresentation = () => {
         <h2 className="text-[#646DF0] font-fusion text-[2rem] font-medium leading-[3rem] text-center
                               lg:text-left lg:text-[2.5rem] lg:leading-[3rem] lg:tracking-wide"
         >O que é o Frontend Fusion</h2>
-        <p className="font-fusion text-base font-normal text-center
+        <p className="w-11/12 font-fusion text-base font-normal text-center
                               lg:text-left"
         >Nossa missão é a criação de uma plataforma de aprendizagem em desenvolvimento backend e frontend gratuita e acessível para todos. Desde 2023, nosso voluntariado já alcançou números incríveis.</p>
 
-        <div className="flex flex-col gap-12 pt-16
-                                lg:pt-6"
-        >
-          <div className="flex gap-6 items-center">
-            <p className="font-fusion text-2xl font-bold leading-9 text-center">+100</p>
-            <p className="font-fusion text-base font-normal text-left">Voluntários ativos no projeto</p>
+        <div className="w-fit flex flex-col gap-12 pt-16 justify-center self-center lg:pt-6 min-[610px]:max-lg:flex-row lg:self-start ">
+          <div className="flex gap-6 items-center justify-start sm:max-lg:justify-center">
+            <p className="font-fusion text-2xl font-bold leading-9">+100</p>
+            <p className="font-fusion text-base font-normal">Voluntários ativos no projeto</p>
           </div>
 
-          <div className="flex gap-6 items-center">
+          <div className="flex gap-6 items-center sm:max-lg:justify-center">
             <p className="font-fusion text-2xl font-bold leading-9 text-center">+300</p>
             <p className="font-fusion text-base font-normal leading-6 text-left">Projetos entregues</p>
           </div>
