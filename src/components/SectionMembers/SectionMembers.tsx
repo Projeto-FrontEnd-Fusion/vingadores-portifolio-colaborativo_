8a0f6df395e790339be056fd6ef4789e7da52d1d
Maria de Fátima <mnunesalves334@gmail.com>
2025-01-21 11:20:02 -0300

    feat: Alinhar e centralizar o título e legenda do carrossel de membros

--- src/components/SectionMembers/SectionMembers.tsx
+++ src/components/SectionMembers/SectionMembers.tsx
@@ -1,13 +1,13 @@
 const SectionMembers = () => {
     return (
-        <div className="flex items-center space-x-40 px-7">
+        <div className="flex flex-row items-center justify-between px-5 py-4 w-full max-w-screen-lg mx-auto">
             <h2 className="text-white text-fusion font text-[32px]">
-                Conheça os <br /> membros do time
+                Conheça os <br />membros do time
             </h2>
 
-            <div className="text-white text-fusion">
-                Lorem ipsum dolor sit amet consectetur. Lacus id cursus <br /> 
-                tincidunt interdum id nisl. Amet risus turpis quam interdum <br /> 
+            <div className="text-white text-fusion text-sm">
+                Lorem ipsum dolor sit amet consectetur. Lacus id cursus<br />
+                tincidunt interdum<br /> id nisl. Amet risus turpis quam interdum
                 magna malesuada.
             </div>
         </div>
