49dcd5ca95d2c766f8a2423c551bdb4f709d2a8b
Maria de Fátima <mnunesalves334@gmail.com>
2025-01-24 13:43:24 -0300

    chore: aplicar melhorias de legibilidade do código

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,43 +1,42 @@
 import { ComponentProps, Dispatch } from "react";
 
 interface ButtonProps extends ComponentProps<"a"> {
-  setIsOpen: Dispatch<boolean>
-};
+  setIsOpen: Dispatch<boolean>;
+}
+
+const buttonClasses = `
+  bg-transparent
+  text-[#26EEEF]
+  font-bold
+  text-lg
+  text-center
+  transition-colors
+  hover:text-[#1A1756]
+  active:text-[#1A9B9D]
+  ease-in-out
+  w-fit
+  h-fit
+  rounded-[2.125rem]
+  py-3
+  px-4
+  mobile-nav:text-sm
+  mobile-nav:bg-[#26EEEF]
+  mobile-nav:text-[#1A1756]
+  mobile-nav:font-normal
+  hover:mobile-nav:bg-[#1CC4D4]
+  active:mobile-nav:bg-[#1A9998]
+`;
 
 export const Button = ({ setIsOpen, ...props }: ButtonProps) => {
-
   return (
     <a
       {...props}
-      className="
-      bg-transparent 
-      text-[#26EEEF] 
-      font-bold
-      text-lg
-      text-center 
-      transition-colors
-      duration-500
-      hover:text-[#1AB8C8]
-      active:text-[#1A9B9D]
-      ease-in-out 
-      w-fit 
-      h-fit 
-      rounded-[2.125rem] 
-      py-3 
-      px-4 
-      mobile-nav:text-sm
-      mobile-nav:bg-[#26EEEF]
-      mobile-nav:text-[#1A1756] 
-      mobile-nav:font-normal 
-      hover:mobile-nav:bg-[#1CC4D4] 
-      active:mobile-nav:bg-[#1A9998] 
-      "
+      className={buttonClasses}
       href="#formulario"
       aria-label="Ir para o formulário de inscrição"
-      onClick={() => { setIsOpen(false) }}
+      onClick={() => setIsOpen(false)}
     >
       Inscreva-se aqui
     </a>
   );
 };
-
