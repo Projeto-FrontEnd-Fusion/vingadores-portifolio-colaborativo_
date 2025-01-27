15fbc58e7e416d703d29e88e4a39cd8e74ef49ff
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-01-26 23:13:06 -0300

    fix: define estilos mobile para o botão na seção de apresentação
    - Devido ao fato do mesmo botão ter estilos diferentes no menu e fora
    dele em dispositivos mobile, foi necessário atribuir o estilo de maneira
    mais explicita. Apenas breakpoints não eram suficientes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,23 @@
 import { ComponentProps, Dispatch } from "react";
+import clsx from "clsx"
 
 interface ButtonProps extends ComponentProps<"a"> {
-  setIsOpen: Dispatch<boolean>;
+  setIsOpen?: Dispatch<boolean>;
+  isNavbar: boolean
 }
 
-export const Button = ({ setIsOpen, ...props }: ButtonProps) => {
+export const Button = ({ setIsOpen, isNavbar, ...props }: ButtonProps) => {
+  const mobileSectionPresentationClasses = "bg-fusion-l-blue text-fusion-d-blue font-normal hover:bg-fusion-l-blue-hover active:bg-fusion-l-blue-active text-fusion-d-blue hover:text-fusion-d-blue"
+
   return (
     <a
       {...props}
-      className="register-button"
+      className={clsx(`register-button`, {
+        [mobileSectionPresentationClasses]: !isNavbar
+      })}
       href="#formulario"
       aria-label="Ir para o formulário de inscrição"
-      onClick={() => setIsOpen(false)}
+      onClick={() => setIsOpen ? setIsOpen(false) : {}}
     >
       Inscreva-se aqui
     </a>
