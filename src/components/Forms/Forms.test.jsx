1cb8455521987fb9ead603e0d8112be1c81b4d16
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-01-28 08:50:06 -0300

    test: finaliza testes do formulário, corrigindo interações com validação

--- src/components/Forms/Forms.test.jsx
+++ src/components/Forms/Forms.test.jsx
@@ -1,27 +1,3 @@
-/**
-  Olá Carol! Aqui vai sua missão!
-
-  Você deve checar se, após o botão de envio for pressionado a mensagem de sucesso/erro é exibida.
-  Eu já deixei tudo certinho para simular o backend durante os testes. Nem precisa rodar o json-server
-
-  Com a função (screen) você consegue encontrar elementos na página
-  através de seus atributos (placeholder, label, etc).
-  Só não dá pra encontrar por classe e id facilmente, é um pouco mais chatinho
-  Mas placeholder e label é suficiente para esse desafio :)
-
-  Ah, a label se refere ao atributo (aria-label), e não ao <label> </label> do html!
-
-  Você consegue interagir com o formulário atráves da função (user)
-  Você vai precisar do type e selectOptions
-
-  Como demora um pouquinho pra ele interagir, você precisa do (await) na frente
-  O (await) para a execução do código e espera até que a função termine de executar
-  
-  Por fim! Procure a mensagem de sucesso/erro na página e se certifique que ele está visivel
-  Algo como: (expect(....).toBeVisible())
-
-  Boa sorte!!
- */
 import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import AxiosMockAdapter from "axios-mock-adapter";
@@ -31,16 +7,10 @@ import { usersInstance } from "../../api/axiosInstance";
 import Forms from "./Forms";
 
 describe("<Forms />", () => {
-  beforeEach(() => {
-    render(<Forms />);
-  });
-  afterEach(cleanup);
-  /*
-  test("deve mostrar mensagem de sucesso se a requisição for bem-sucedida", async () => {
-    const mock = new AxiosMockAdapter(usersInstance);
-    mock.onPost("/users").reply(200);
+  let user = userEvent.setup();
 
-    const user = userEvent.setup();
+  beforeEach(async () => {
+    render(<Forms />);
 
     await user.type(screen.getByLabelText("Nome"), "Caroline");
     await user.type(screen.getByLabelText("Sobrenome"), "Moraes");
@@ -52,39 +22,31 @@ describe("<Forms />", () => {
     const select = screen.getByLabelText("Vaga desejada");
     await user.selectOptions(select, "Desenvolvedor Frontend");
 
-    await user.type(screen.getByLabelText("Conte-nos sobre você"), "Descrição");
+    await user.type(screen.getByLabelText("Conte-nos sobre você"), "Desenvolvedora Frontend, membra da Squad Vingadores na Comunidade Frontend Fusion");
+  });
+
+  test("deve mostrar mensagem de sucesso se a requisição for bem-sucedida", async () => {
+    const mock = new AxiosMockAdapter(usersInstance);
+    mock.onPost("/users").reply(200);
 
     await user.click(
-      screen.getByText(/Enviar formulário/i, { selector: "button" })
+      screen.getByText("Enviar formulário")
     );
 
     const successMessage = await screen.findByText(
-      "Seja bem-vindo(a) à Comunidade Frontend Fusion!\n Cheque sua caixa de entrada para validar seu email."
+      "Seja bem-vindo(a) à Comunidade Frontend Fusion! Cheque sua caixa de entrada para validar seu email."
     );
 
-    expect(successMessage).toBeVisible(); 
+    expect(successMessage).toBeVisible();
   });
-  */
 
   test("deve mostrar mensagem de erro se a requisição for mal sucedida", async () => {
     const mock = new AxiosMockAdapter(usersInstance);
     mock.onPost("/users").reply(404);
 
-    const user = userEvent.setup();
-
-    await user.type(screen.getByLabelText("Nome"), "Caroline");
-    await user.type(screen.getByLabelText("Sobrenome"), "Moraes");
-    await user.type(screen.getByLabelText("E-mail"), "caroline@example.com");
-
-    const select = screen.getByLabelText("Vaga desejada");
-    await user.selectOptions(select, "Desenvolvedor Frontend");
-
-    await user.type(screen.getByLabelText("Conte-nos sobre você"), "Descrição");
-
     await user.click(screen.getByRole("button", { name: "Enviar formulário" }));
-    screen.debug();
 
-    const errorMessage = await screen.findByText("Oops, ocorreu um erro.");
-    expect(errorMessage).toBeVisible();
+    const errorMessage = await screen.findByText("Oops, ocorreu um erro. Tente novamente!");
+    expect(errorMessage).toBeVisible()
   });
 });
