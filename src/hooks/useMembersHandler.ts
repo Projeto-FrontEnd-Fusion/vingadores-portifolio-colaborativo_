5656a2ec82f36506686be3170b32d34997111c5e
Caroline de Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-02-12 16:00:08 +0000

    Armazena dados simulados em uma Store Zustand

--- src/hooks/useMembersHandler.ts
+++ src/hooks/useMembersHandler.ts
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { useGetMember } from "../api/membersApi/membersApi";
+import { useUserStore } from "../store/useUserStore";
 
 const useMembersHandler = () => {
   const { data: members, error, isLoading } = useGetMember();
+  const setUsers = useUserStore((state) => state.setUsers);
+
+
+  useEffect(() => {
+    if (members) {
+      setUsers(members);
+    }
+  }, [members, setUsers]);
 
   return { members, error, isLoading };
-}
+};
 
 export default useMembersHandler;
