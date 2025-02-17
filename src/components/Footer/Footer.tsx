d8d3d431d26c1bfa7ab84b2c9cb1bebdc75a42eb
Caroline de Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-02-17 16:11:05 +0000

    feat: Adiconando links de redes sociais

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
+
 const Footer = () => {
     return (
+
         <footer className="flex flex-col p-6 pb-24">
             <hr className="border-t border-gray-300 mb-4" />
 
@@ -8,7 +10,7 @@ const Footer = () => {
                     <img src="/assets/logotipo_frontend_fusion.png" alt="Logotipo Frontend Fusion" />
                     <ul className="flex flex-row space-x-4">
                         <li>
-                            <a href="#" className="text-fusion-links-rodape">Contato</a>
+                            <a href="mailto:projetofrontendfusion@gmail.com" className="text-fusion-links-rodape">Contato</a>
                         </li>
                         <li>
                             <a href="#" className="text-fusion-links-rodape">Política de Privacidade</a>
@@ -19,8 +21,23 @@ const Footer = () => {
                 <span className="text-fusion-rodape-redes-sociais self-end">
                     Acompanhe nossas redes sociais
                 </span>
-            </div>
-        </footer>        
+                <ul className="flex space-x-4 mt-2">
+                    <li>
+                        <a href="https://www.linkedin.com/company/comunidade-frontend-fusion/" target="_blank" rel="noopener noreferrer" className="text-fusion-links-rodape">
+                            <i className="devicon-linkedin-plain text-blue-600"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Projeto-FrontEnd-Fusion" target="_blank" rel="noopener noreferrer" className="text-fusion-links-rodape">
+                            <i className="devicon-github-original text-white"></i>
+                        </a>
+                    </li>
+                </ul>
+
+            </div >
+
+        </footer >
+
     );
 }
 
