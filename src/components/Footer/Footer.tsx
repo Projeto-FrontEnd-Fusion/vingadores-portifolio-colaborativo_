2460ff31e6680317b7813d92ccba03d023cc36db
Caroline de Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-02-19 16:17:47 +0000

    feat: refatora codigo e ajusta responsividade

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,41 @@
 const Footer = () => {
     return (
-        <footer className="flex flex-col p-6 pb-24">
+        <footer className="p-6 pb-24">
             <hr className="border-t border-gray-300 mb-4" />
 
-            <div className="flex justify-between items-center">
-                <nav className="flex items-center space-x-6" aria-label="Navegação principal">
-                    <img src="/assets/logotipo_frontend_fusion.png" alt="Logotipo Frontend Fusion" />
-                    <ul className="flex flex-row space-x-4">
+            <nav className="flex flex-col md:flex-row justify-between w-full items-start md:items-center" aria-label="Rodapé">
+
+
+                <div className="flex flex-col md:flex-row md:items-center md:space-x-6 w-full md:w-auto">
+                    <img src="/assets/logotipo_frontend_fusion.png" alt="Logotipo Frontend Fusion" className="w-40 self-start md:self-auto" />
+
+                    <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 self-start md:self-auto">
                         <li>
-                            <a href="mailto:projetofrontendfusion@gmail.com" className="text-gray-400">Contato</a>
+                            <a href="mailto:projetofrontendfusion@gmail.com" className="text-gray-400" target="_blank" rel="noopener noreferrer" aria-label="E-mail">Contato</a>
                         </li>
                         <li>
                             <a href="https://seasoned-toucan-88d.notion.site/Pol-tica-de-Privacidade-do-Projeto-Frontend-Fusion-dd0ee78762374f89a395a54ac20407fe" target="_blank" rel="noopener noreferrer" className="text-gray-400">Política de Privacidade</a>
                         </li>
                     </ul>
-                </nav>
-                <section className="flex items-center space-x-4" aria-label="Redes sociais">
-                    <span className="text-gray-400">Acompanhe nossas redes sociais</span>
+                </div>
+
+                <div className="flex flex-col md:flex-row items-start md:items-center mt-4 md:mt-0 gap-2 md:gap-4">
+                    <span className="text-gray-400 text-left">Acompanhe nossas redes sociais</span>
                     <ul className="flex space-x-4">
                         <li>
-                            <a href="https://www.linkedin.com/company/comunidade-frontend-fusion/" target="_blank" rel="noopener noreferrer" className="text-fusion-links-rodape" aria-label="LinkedIn">
+                            <a href="https://www.linkedin.com/company/comunidade-frontend-fusion/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                                 <i className="devicon-linkedin-plain text-fusion-purple text-2xl"></i>
                             </a>
                         </li>
                         <li>
-                            <a href="https://github.com/Projeto-FrontEnd-Fusion" target="_blank" rel="noopener noreferrer" className="text-fusion-links-rodape" aria-label="GitHub">
+                            <a href="https://github.com/Projeto-FrontEnd-Fusion" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                                 <i className="devicon-github-original text-fusion-purple text-2xl"></i>
                             </a>
                         </li>
                     </ul>
-                </section>
-            </div>
+                </div>
+
+            </nav>
         </footer>
     );
 }
