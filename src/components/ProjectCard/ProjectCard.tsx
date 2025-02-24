bed51e4c67a3642fb28d15eecef5470c1edce8c1
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-02-24 20:40:04 -0300

    feat: ajusta responsividade do componente `<ProjectCard />`

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -9,21 +9,19 @@ interface Project {
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="flex mx-[14%] gap-6">
+    <div className="flex flex-col gap-6 mobile-nav:flex-row mobile-nav:justify-center">
       <img
         src={project.project_cover || "https://via.placeholder.com/400"}
-        className="w-1/2 rounded-md"
+        className="w-auto aspect-auto h-auto rounded-md max-h-80"
         alt={project.project_name}
       />
-
-      <div className="flex flex-col gap-3 py-6 text-white">
+      <div className="flex flex-col text-center gap-3 py-6 text-white mobile-nav:w-2/5 mobile-nav:text-left">
         <h4 className="text-xl font-bold">{project.project_name}</h4>
         <p>{project.description}</p>
 
         <a href={project.link || "#"} target="_blank" rel="noopener noreferrer" >
           MAIS DETALHES
         </a>
-
         <hr />
       </div>
     </div>
