d00dca50f61e2caeba219311488e6ec413e7dcea
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2025-02-28 14:49:47 -0300

    chore: re-configura vitest e tsconfig, adicionando matchers do `jest-dom`

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: './vitest.setup.js',
-    environment: 'jsdom'
+    setupFiles: ['./vitest.setup.ts'],
+    environment: 'jsdom',
   }
 })
